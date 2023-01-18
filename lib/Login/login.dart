3217754021cd95ec1413742e3f83cc9333f4f169
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2023-01-18 12:39:00 -0300

    feat: add loading indicator on login screen

--- lib/Login/login.dart
+++ lib/Login/login.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: sized_box_for_whitespace, prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:gradient_borders/gradient_borders.dart';
 
 class Login extends StatefulWidget {
@@ -12,6 +13,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool hidePassword = false;
+  bool isLoading = false;
 
   @override
   void initState() {
@@ -26,85 +29,112 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(widget.context).size.width * 0.05),
+      padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(widget.context).size.width * 0.05),
       child: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-        TextField(
-          decoration: InputDecoration(
-              suffixIcon: Icon(Icons.person),
-              contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              border: GradientOutlineInputBorder(
-                gradient: LinearGradient(
-                    colors: [Colors.indigo, Colors.purple, Colors.red]),
-                width: 2,
-                borderRadius: BorderRadius.circular(50)
-              ),
-              focusedBorder: GradientOutlineInputBorder(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                  suffixIcon: ShaderMask(
+                      blendMode: BlendMode.srcIn,
+                      shaderCallback: (Rect bounds) => RadialGradient(
+                            center: Alignment.topCenter,
+                            colors: [Colors.indigo, Colors.purple, Colors.red],
+                          ).createShader(bounds),
+                      child: Icon(Icons.person)),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  border: GradientOutlineInputBorder(
+                      gradient: LinearGradient(
+                          colors: [Colors.indigo, Colors.purple, Colors.red]),
+                      width: 2,
+                      borderRadius: BorderRadius.circular(50)),
+                  focusedBorder: GradientOutlineInputBorder(
+                      gradient: LinearGradient(
+                          colors: [Colors.indigo, Colors.purple, Colors.red]),
+                      width: 2,
+                      borderRadius: BorderRadius.circular(50)),
+                  label: Text(
+                    "Digite seu nome de usuário",
+                    style: TextStyle(fontSize: 13),
+                  )),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.045,
+            ),
+            TextField(
+              obscureText: hidePassword,
+              obscuringCharacter: '●',
+              decoration: InputDecoration(
+                  suffixIcon: ShaderMask(
+                      blendMode: BlendMode.srcIn,
+                      shaderCallback: (Rect bounds) => RadialGradient(
+                            center: Alignment.topCenter,
+                            colors: [Colors.indigo, Colors.purple, Colors.red],
+                          ).createShader(bounds),
+                      child: InkWell(
+                          onTap: () => setState(() {
+                                hidePassword = !hidePassword;
+                              }),
+                          child: Icon(hidePassword
+                              ? Icons.visibility_off
+                              : Icons.remove_red_eye_sharp))),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                  border: GradientOutlineInputBorder(
+                      gradient: LinearGradient(
+                          colors: [Colors.indigo, Colors.purple, Colors.red]),
+                      width: 2,
+                      borderRadius: BorderRadius.circular(50)),
+                  focusedBorder: GradientOutlineInputBorder(
+                      gradient: LinearGradient(
+                          colors: [Colors.indigo, Colors.purple, Colors.red]),
+                      width: 2,
+                      borderRadius: BorderRadius.circular(50)),
+                  label: Text(
+                    "Digite sua senha",
+                    style: TextStyle(fontSize: 13),
+                  )),
+            ),
+            InkWell(
+              onTap: () async {
+                setState(() => (isLoading = true));
+                await Future.delayed(Duration(seconds: 3)); //Aqui vai a função para login
+                setState(() => (isLoading = false));
+              },
+              child: Container(
+                margin: EdgeInsets.only(top: 40),
+                height: MediaQuery.of(context).size.height * 0.075,
+                width: MediaQuery.of(context).size.width * 0.75,
+                decoration: BoxDecoration(
                   gradient: LinearGradient(
                       colors: [Colors.indigo, Colors.purple, Colors.red]),
-                  width: 2,
-                  borderRadius: BorderRadius.circular(50)),
-              label: Text(
-                "Digite seu nome de usuário",
-                style: TextStyle(
-                  fontSize: 13
+                  borderRadius: BorderRadius.circular(50),
                 ),
-              )
-          ),
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.045,
-        ),
-        TextField(
-          obscureText: true,
-          obscuringCharacter: '●',
-          decoration: InputDecoration(
-              suffixIcon: Icon(Icons.remove_red_eye),
-              contentPadding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-              border: GradientOutlineInputBorder(
-                gradient: LinearGradient(
-                    colors: [Colors.indigo, Colors.purple, Colors.red]),
-                width: 2,
-                borderRadius: BorderRadius.circular(50)
+                child: Center(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    isLoading
+                        ? SpinKitRing(
+                            color: Colors.white,
+                            size: 27,
+                            lineWidth: 3,
+                          )
+                        : Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 19,
+                                fontWeight: FontWeight.bold),
+                          ),
+                  ],
+                )),
               ),
-              focusedBorder: GradientOutlineInputBorder(
-                  gradient: LinearGradient(
-                      colors: [Colors.indigo, Colors.purple, Colors.red]),
-                  width: 2,
-                  borderRadius: BorderRadius.circular(50)),
-              label: Text(
-                "Digite sua senha",
-                style: TextStyle(
-                  fontSize: 13
-                ),
-              )
-          ),
-        ),
-        InkWell(
-          onTap: () {},
-          child: Container(
-            margin: EdgeInsets.only(top: 40),
-            height: MediaQuery.of(context).size.height * 0.075,
-            width: MediaQuery.of(context).size.width * 0.75,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [Colors.indigo, Colors.purple, Colors.red]),
-              borderRadius: BorderRadius.circular(50),
-            ),
-            child: Center(
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 19,
-                  fontWeight: FontWeight.bold
-                ),
-              )
-            ),
-          ),
-        )
-      ]),
+            )
+          ]),
     );
   }
 }
